0367699129447d43de5a0547e7ff2ff457b0faad
Henry Letting <lettinghenry0@gmail.com>
2025-11-04 17:41:27 +0300

    implemented a broadcast system where all clients receive the same synchronized stream

--- hello-world/main.go
+++ hello-world/main.go
@@ -4,16 +4,59 @@ import (
     "flag"
     "fmt"
     "net/http"
+    "sync"
     "time"
 )
 
 var addr = flag.String("addr",":8080","HTTP network address")
+
+// Global broadcast system
+type Broadcaster struct {
+    clients map[chan string]bool
+    mutex   sync.RWMutex
+}
+
+func NewBroadcaster() *Broadcaster {
+    return &Broadcaster{
+        clients: make(map[chan string]bool),
+    }
+}
+
+func (b *Broadcaster) AddClient(client chan string) {
+    b.mutex.Lock()
+    defer b.mutex.Unlock()
+    b.clients[client] = true
+}
+
+func (b *Broadcaster) RemoveClient(client chan string) {
+    b.mutex.Lock()
+    defer b.mutex.Unlock()
+    delete(b.clients, client)
+    close(client)
+}
+
+func (b *Broadcaster) Broadcast(message string) {
+    b.mutex.RLock()
+    defer b.mutex.RUnlock()
+    for client := range b.clients {
+        select {
+        case client <- message:
+        default:
+            // Client channel is full, skip
+        }
+    }
+}
+
+var broadcaster = NewBroadcaster()
 
 
 func main() {
     testPrint()
 
     flag.Parse()
+
+    // Start the global message broadcaster
+    go startGlobalBroadcast()
 
     http.HandleFunc("/", home)
     http.HandleFunc("/stream", streamHandler)
@@ -24,6 +67,21 @@ func main() {
         fmt.Println("Error:", err)
    }
 
+}
+
+func startGlobalBroadcast() {
+    ticker := time.NewTicker(1 * time.Second)
+    defer ticker.Stop()
+    
+    counter := 0
+    for {
+        select {
+        case <-ticker.C:
+            counter++
+            message := fmt.Sprintf("Message %d at %s", counter, time.Now().Format("15:04:05"))
+            broadcaster.Broadcast(message)
+        }
+    }
 }
 
 func home(w http.ResponseWriter, r *http.Request){
@@ -37,31 +95,38 @@ func streamHandler(w http.ResponseWriter, r *http.Request) {
     w.Header().Set("Cache-Control", "no-cache")
     w.Header().Set("Connection", "keep-alive")
     w.Header().Set("Access-Control-Allow-Origin", "*")
+    w.Header().Set("X-Accel-Buffering", "no") // Disable nginx buffering
+    
+    // Ensure we can flush
+    flusher, ok := w.(http.Flusher)
+    if !ok {
+        http.Error(w, "Streaming unsupported", http.StatusInternalServerError)
+        return
+    }
+
+    // Create client channel and register with broadcaster
+    clientChan := make(chan string, 10) // Buffer to prevent blocking
+    broadcaster.AddClient(clientChan)
+    defer broadcaster.RemoveClient(clientChan)
+
+    // Send initial connection message
+    fmt.Fprintf(w, "data: Connected to synchronized stream\n\n")
+    flusher.Flush()
 
     // Create a channel to signal when client disconnects
     clientGone := r.Context().Done()
 
-    fmt.Println("Client connected to stream")
+    fmt.Println("Client connected to synchronized stream")
 
-    // Send data every second
-    ticker := time.NewTicker(1 * time.Second)
-    defer ticker.Stop()
-
-    counter := 0
     for {
         select {
         case <-clientGone:
             fmt.Println("Client disconnected from stream")
             return
-        case <-ticker.C:
-            counter++
+        case message := <-clientChan:
             // Send SSE formatted data
-            fmt.Fprintf(w, "data: Message %d at %s\n\n", counter, time.Now().Format("15:04:05"))
-            
-            // Flush the data to client immediately
-            if flusher, ok := w.(http.Flusher); ok {
-                flusher.Flush()
-            }
+            fmt.Fprintf(w, "data: %s\n\n", message)
+            flusher.Flush()
         }
     }
 }
