d7d83196cfaa71eb3cabe55f7f9933eb5670ae0b
Henry Letting <lettinghenry0@gmail.com>
2025-11-04 16:59:05 +0300

    added a streaming route

--- hello-world/main.go
+++ hello-world/main.go
@@ -4,6 +4,7 @@ import (
     "flag"
     "fmt"
     "net/http"
+    "time"
 )
 
 var addr = flag.String("addr",":8080","HTTP network address")
@@ -15,8 +16,9 @@ func main() {
     flag.Parse()
 
     http.HandleFunc("/", home)
+    http.HandleFunc("/stream", streamHandler)
     fmt.Println("Server running on", *addr)
-   err := http.ListenAndServe(*addr, nil)
+    err := http.ListenAndServe(*addr, nil)
 
    if err!= nil{
         fmt.Println("Error:", err)
@@ -27,6 +29,41 @@ func main() {
 func home(w http.ResponseWriter, r *http.Request){
         fmt.Fprintln(w,"Hello World!")
         fmt.Println("called /home")
+}
+
+func streamHandler(w http.ResponseWriter, r *http.Request) {
+    // Set headers for Server-Sent Events
+    w.Header().Set("Content-Type", "text/event-stream")
+    w.Header().Set("Cache-Control", "no-cache")
+    w.Header().Set("Connection", "keep-alive")
+    w.Header().Set("Access-Control-Allow-Origin", "*")
+
+    // Create a channel to signal when client disconnects
+    clientGone := r.Context().Done()
+
+    fmt.Println("Client connected to stream")
+
+    // Send data every second
+    ticker := time.NewTicker(1 * time.Second)
+    defer ticker.Stop()
+
+    counter := 0
+    for {
+        select {
+        case <-clientGone:
+            fmt.Println("Client disconnected from stream")
+            return
+        case <-ticker.C:
+            counter++
+            // Send SSE formatted data
+            fmt.Fprintf(w, "data: Message %d at %s\n\n", counter, time.Now().Format("15:04:05"))
+            
+            // Flush the data to client immediately
+            if flusher, ok := w.(http.Flusher); ok {
+                flusher.Flush()
+            }
+        }
+    }
 }
 
 func testPrint(){
